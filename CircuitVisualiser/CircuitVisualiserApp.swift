bf0ccfb67d8fe95416d2e8ba1c5df884747d3d87
Alex Albu <alex.albu@tss-yonder.com>
2024-01-24 17:51:12 +0200

    Initialise VisionPro project

---
+++ CircuitVisualiser/CircuitVisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitVisualiserApp.swift
+//  CircuitVisualiser
+//
+//  Created by Alex Albu on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CircuitVisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
